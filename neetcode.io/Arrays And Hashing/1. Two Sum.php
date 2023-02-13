ba698bc5451e9b23dd761d0e87ba3b3f070580ff
Ali Yousefi <aliyousefi.tec@gmail.com>
2023-02-13 04:21:30 +0330

    Update better solution Two Sum

--- neetcode.io/Arrays And Hashing/1. Two Sum.php
+++ neetcode.io/Arrays And Hashing/1. Two Sum.php
@@ -1,5 +1,6 @@
 <?php
 
+// Time = O(n^2)
 function twoSum($nums, $target)
 {
     $bound = count($nums);
@@ -13,5 +14,21 @@ function twoSum($nums, $target)
                 return [$i, $j];
             }
         }
+    }
+}
+
+// Time = O(n)
+function twoSumV2($nums, $target)
+{
+    $bound = count($nums);
+    $array = [];
+    for ($i = 0; $i < $bound; $i++) {
+        $remind = $target - $nums[$i];
+
+        if(isset($array[$remind])) {
+            return [$array[$remind], $i];
+        }
+
+        $array[$nums[$i]] = $i;
     }
 }
